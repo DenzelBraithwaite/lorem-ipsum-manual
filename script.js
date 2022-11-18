121f5e73b2cc604d8b559e9eb8e61c36724bc06e
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-11-18 14:50:11 -0500

    added hidden class that can be toggled

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const sectionHome = document.querySelector('.section-home');
+const btnHome = document.getElementById('btn-home');
+btnHome.addEventListener('click', () => {
+    console.log('clicked!');
+    sectionHome.classList.toggle('hidden');
+});
