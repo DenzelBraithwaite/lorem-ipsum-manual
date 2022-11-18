d4a308e6e8c4d86dc1ccbbcc9f32ba539f2d287f
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-11-18 16:08:50 -0500

    implemented rough draft of section visibility toggling with js

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 'use strict';
 
-const sectionHome = document.querySelector('.section-home');
-const btnHome = document.getElementById('btn-home');
-btnHome.addEventListener('click', () => {
-    console.log('clicked!');
-    sectionHome.classList.toggle('hidden');
+// All elements with the 'hidden' class
+
+const allSections = document.querySelectorAll('.hidden');
+// Show Homepage
+document.querySelector('.section-0').classList.remove('hidden');
+
+// All navbar buttons
+const allNavBtns = document.querySelectorAll('.btn-nav');
+
+// Hide all elements
+const hideElements = function () {
+    allSections.forEach(section => {
+        if (!section.classList.contains('hidden')) {
+            section.classList.add('hidden');
+        }
+    });
+};
+
+// Toggle section visibility
+allNavBtns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        hideElements();
+        console.log(event.target.isEqualNode(allNavBtns[0]));
+        for (let i = 0; i < allNavBtns.length; i++) {
+            if (event.target.isEqualNode(allNavBtns[i])) {
+                allSections[i].classList.remove('hidden');
+            }
+        }
+    });
 });
